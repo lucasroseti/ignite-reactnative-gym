ba83a8ef4f3f8d2cdbc6291e5be4caf9590de908
Lucas Roseti <lc.roseti@gmail.com>
2024-04-03 20:28:37 -0300

    HomeHeader and UserPhoto component

---
+++ src/components/HomeHeader.tsx
@@ -0,0 +1,21 @@
+import { Heading, HStack, Text, VStack } from 'native-base'
+
+import { UserPhoto } from './UserPhoto'
+
+export function HomeHeader() {
+  return (
+    <HStack bg="gray.600" pt={16} pb={5} px={8} alignItems="center">
+      <UserPhoto
+        source={{ uri: 'https://github.com/lucasroseti.png' }}
+        size={16}
+        alt="user image"
+        mr={4}
+      />
+
+      <VStack>
+        <Text color="gray.100" fontSize="md">Olá,</Text>
+        <Heading color="gray.100" fontSize="md">Rodrigo</Heading>
+      </VStack>
+    </HStack>
+  )
+}
