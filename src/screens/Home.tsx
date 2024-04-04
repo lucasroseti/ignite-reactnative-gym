554e3113056d8c3eeb94d51b80d297ce8f6979f0
Lucas Roseti <lc.roseti@gmail.com>
2024-04-03 21:10:19 -0300

    ExerciseCard component

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,8 +1,9 @@
 import { useState } from 'react'
-import { FlatList, HStack, VStack } from 'native-base'
+import { FlatList, HStack, Heading, Text, VStack } from 'native-base'
 
-import { HomeHeader } from '@components/HomeHeader'
+import { ExerciseCard } from '@components/ExerciseCard'
 import { Group } from '@components/Group'
+import { HomeHeader } from '@components/HomeHeader'
 
 export function Home() {
   const [groups, setGroups] = useState<string[]>(['back','bicpes', 'triceps', 'shoulder'])
@@ -28,6 +29,16 @@ export function Home() {
         my={10}
         maxH={10}
       />
+
+      <VStack flex={1} px={8}>
+        <HStack justifyContent="space-between" mb={5}>
+          <Heading color="gray.200" fontSize="md">Exercises</Heading>
+          <Text color="gray.200" fontSize="sm">4</Text>
+        </HStack>
+
+        <ExerciseCard />
+        <ExerciseCard />
+      </VStack>
     </VStack>
   )
 }
