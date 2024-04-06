0b8e76812b20995f45dc5754d4ddaeed6f1471d4
Lucas Roseti <lc.roseti@gmail.com>
2024-04-06 15:07:24 -0300

    Fetching groups

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,23 +1,46 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useNavigation } from '@react-navigation/native'
-import { FlatList, HStack, Heading, Text, VStack } from 'native-base'
+import { FlatList, HStack, Heading, Text, VStack, useToast } from 'native-base'
 
+import { api } from '@services/api'
 import { AppNavigatorRoutesProps } from '@routes/app.routes'
+import { AppError } from '@utils/AppError'
 
 import { ExerciseCard } from '@components/ExerciseCard'
 import { Group } from '@components/Group'
 import { HomeHeader } from '@components/HomeHeader'
 
 export function Home() {
-  const [groups, setGroups] = useState<string[]>(['back','biceps', 'triceps', 'shoulder'])
+  const [groups, setGroups] = useState<string[]>([])
   const [exercises, setExercises] = useState<string[]>(['One arm dumbbell row','Pull-up', 'Pullovers', 'Deadlifts'])
   const [groupSelected, setGroupSelected] = useState('back')
 
   const navigation = useNavigation<AppNavigatorRoutesProps>()
+  const toast = useToast()
+
+  async function fetchGroups() {
+    try {
+      const { data } = await api.get('/groups')
+      setGroups(data)
+    } catch (error) {
+      const isAppError = error instanceof AppError
+      const title = isAppError ? error.message : 'Unable to loade muscle groups'
+
+      toast.show({
+        title,
+        placement: 'top',
+        bgColor: 'red.500'
+      })
+    }
+  }
 
   function handleOpenExerciseDetails() {
     navigation.navigate('exercise')
   }
+
+  useEffect(() => {
+    fetchGroups()
+  }, [])
 
   return (
     <VStack flex={1}>
