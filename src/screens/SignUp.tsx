8c380b187eb2079f84392235a041fe2932645127
Lucas Roseti <lc.roseti@gmail.com>
2024-04-05 21:14:46 -0300

    Axios, interceptors and error handling

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,8 +1,11 @@
 import { Controller, useForm } from 'react-hook-form'
 import { useNavigation } from '@react-navigation/native'
-import { Center, Heading, Image, ScrollView, Text, VStack } from 'native-base'
+import { Center, Heading, Image, ScrollView, Text, VStack, useToast } from 'native-base'
 import * as yup from 'yup'
 import { yupResolver } from '@hookform/resolvers/yup'
+
+import { api } from '@services/api'
+import { AppError } from '@utils/AppError'
 
 import { Button } from '@components/Button'
 import { Input } from '@components/Input'
@@ -30,24 +33,26 @@ export function SignUp() {
     resolver: yupResolver(signUpSchema),
   })
 
+  const toast = useToast()
+
   function handleGoBack() {
     navigation.goBack()
   }
 
   async function handleSignUp({ name, email, password }: FormDataProps) {
-    const body = JSON.stringify({ name, email, password })
+    try {
+      const response = await api.post('users', { name, email, password })
+      console.log(response.data)
+    } catch (error) {
+      const isAppError = error instanceof AppError
+      const title = isAppError ? error.message : 'Unable to create account. Try again later'
 
-    const response = await fetch('http://192.168.15.27:3333/users', {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      body
-    })
-
-    const data = await response.json()
-    console.log(data)
+      toast.show({
+        title,
+        placement: 'top',
+        bgColor: isAppError ? 'yellow.600' : 'red.500'
+      })
+    }
   }
 
   return (
